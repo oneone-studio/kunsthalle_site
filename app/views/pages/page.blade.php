fd67b4bcb41272e3d2c5a1effb29b50c1f8029ab
shahid <shahidm08@gmail.com>
2019-01-29 16:51:55 +0100

    Updated page

--- app/views/pages/page.blade.php
+++ app/views/pages/page.blade.php
@@ -10,7 +10,7 @@
                     <?php $size = 's';
                         if($t->size) { $size = strtolower($t->size); }
                     ?>
-                       <span class="text-{{$size}}">{{$t->line}}</span>
+                       <span class="text-{{$size}}">{{$t->line_de}}</span>
                    @endforeach
                 @endif    
                 </h1>
